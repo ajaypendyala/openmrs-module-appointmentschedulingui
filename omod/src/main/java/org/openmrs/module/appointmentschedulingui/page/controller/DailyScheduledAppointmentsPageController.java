b7a30680486d76eacb38b33e6704c0199527aa0b
Ajay <ajaychandrapendyala@gmail.com>
2017-11-15 03:13:57 -0500

    Update DailyScheduledAppointmentsPageController.java

--- omod/src/main/java/org/openmrs/module/appointmentschedulingui/page/controller/DailyScheduledAppointmentsPageController.java
+++ omod/src/main/java/org/openmrs/module/appointmentschedulingui/page/controller/DailyScheduledAppointmentsPageController.java
@@ -7,8 +7,12 @@ import org.openmrs.module.appui.UiSessionContext;
 import org.openmrs.module.emrapi.EmrApiConstants;
 import org.openmrs.ui.framework.annotation.SpringBean;
 import org.openmrs.ui.framework.page.PageModel;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 public class DailyScheduledAppointmentsPageController {
+    
+     protected final Log log = LogFactory.getLog(getClass());
 
     public void get(PageModel pageModel,
                     @SpringBean("appointmentSchedulingUIProperties")AppointmentSchedulingUIProperties properties,
@@ -21,5 +25,11 @@ public class DailyScheduledAppointmentsPageController {
        pageModel.addAttribute("sessionLocationUuid", location != null ? location.getUuid() : "");
        String telephoneAttributeTypeName = EmrApiConstants.TELEPHONE_ATTRIBUTE_TYPE_NAME;
        pageModel.addAttribute("telephoneAttributeTypeName", telephoneAttributeTypeName);
+        
+        
+        log.info("Daily appointments viewed at location (session) " + location + "UUID " +location.getUuid());
+        
+        
+        
     }
 }
